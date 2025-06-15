b107c5735ef2dcc3aaf39f0b1c8477fb147bd910
Guillaume Bellec <guillaume.bellec.eu@gmail.com>
2025-06-15 02:59:37 +0200

    sync

--- run_slurm.sh
+++ run_slurm.sh
@@ -11,5 +11,12 @@
 #SBATCH --mail-user=guillaume.bellec@tuwien.ac.at
 #SBATCH --mail-type=END,FAIL
 
+# Activate conda environment
+source ~/.bashrc  # or wherever conda is initialized
 conda activate tiny-gpt
+
+# Set some useful environment variables
+#export MASTER_ADDR=$(hostname)
+#export MASTER_PORT=12355
+
 torchrun --standalone --nproc_per_node=4 main.py
