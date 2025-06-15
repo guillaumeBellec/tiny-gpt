439195ded4889aa783cd0d07409ff5f9a6dd271c
Guillaume Bellec <guillaume.bellec.eu@gmail.com>
2025-06-15 02:53:15 +0200

    sync

--- run_slurm.sh
+++ run_slurm.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=GPU-h100
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
-#SBATCH --time=02:00:00
+#SBATCH --time=01:00:00
 #SBATCH --output=last_dist_log.txt
 #SBATCH --gres=gpu:h100:4
 #SBATCH --cpus-per-task=32
@@ -11,4 +11,5 @@
 #SBATCH --mail-user=guillaume.bellec@tuwien.ac.at
 #SBATCH --mail-type=END,FAIL
 
+conda activate tiny-gpt
 torchrun --standalone --nproc_per_node=4 main.py
