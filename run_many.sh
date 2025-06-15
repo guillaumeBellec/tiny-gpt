7d8902e800fb0e099411d168f7858df774409cbe
Guillaume Bellec <guillaume.bellec.eu@gmail.com>
2025-06-15 03:36:43 +0200

    sync

--- run_many.sh
+++ run_many.sh
@@ -1,7 +1,8 @@
-python main.py --lr3=0.0001
-python main.py --lr3=0.0003
-python main.py --lr3=0.001 --wd=0.01
-python main.py --lr3=0.001 --wd=0.1
-python main.py --lr3=0.003
-python main.py --lr3=0.01
-python main.py --lr3=0.03
+#!/bin/bash
+#python main.py --distributed=0 --lr3=0.0001
+python main.py --distributed=0 --lr3=0.0003
+python main.py --distributed=0 --lr3=0.001 --wd=0.01
+python main.py --distributed=0 --lr3=0.001 --wd=0.1
+python main.py --distributed=0 --lr3=0.003
+python main.py --distributed=0 --lr3=0.01
+python main.py --distributed=0 --lr3=0.03
