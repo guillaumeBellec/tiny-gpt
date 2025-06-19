59afd0c1353c3f9c3c46b61feb5fc40cc480272e
Guillaume Bellec <guillaume.bellec.eu@gmail.com>
2025-06-19 14:14:18 +0200

    sync

--- run_many.sh
+++ run_many.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #python main.py --distributed=0 --lr3=0.0001
-python main.py --distributed=0 --lr3=0.002 --wd=0.1
-python main.py --distributed=0 --lr3=0.002 --wd=0.1 --scheduler_type=warmup
-python main.py --distributed=0 --lr3=0.002 --wd=0.1 --scheduler_type=cosine
+python main.py --distributed=0 --lr3=0.0002
+python main.py --distributed=0 --lr3=0.0005
+python main.py --distributed=0 --lr3=0.001
+python main.py --distributed=0 --lr3=0.0002 --wd=0.1 --scheduler_type=null
+python main.py --distributed=0 --lr3=0.0001 --wd=0.1 --scheduler_type=null
